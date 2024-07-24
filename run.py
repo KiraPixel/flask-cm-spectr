15b75e6d4970e5f8a201b62ab0e3c99300306735
Kira <90696175+KiraPixel@users.noreply.github.com>
2024-07-24 19:22:48 +0300

    docker fuckbrains

--- run.py
+++ run.py
@@ -3,20 +3,4 @@ import time
 from app import create_app
 from app.modules import DBcash
 
-def update_db_periodically():
-    while True:
-        DBcash.UpdateBD()
-        time.sleep(60)  # ждем 60 секунд
-
-
-# Создаем и запускаем поток для обновления БД
-update_thread = threading.Thread(target=update_db_periodically)
-update_thread.daemon = True  # делаем поток демоном, чтобы он завершался при завершении основного потока
-update_thread.start()
-
-# Создаем приложение
-app = create_app()
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0')
+print('test')
