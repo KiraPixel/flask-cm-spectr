e03b1f733afa923a7159925a364fd0bcf1a64a46
Kira <90696175+KiraPixel@users.noreply.github.com>
2024-08-14 16:46:04 +0300

    fix

--- run.py
+++ run.py
@@ -4,12 +4,11 @@ import time
 from datetime import datetime
 from app import create_app
 
-
 # Создаем приложение
 app = create_app()
 
 if __name__ == '__main__':
     app.debug = True
     HOST = os.getenv('HOST', '0.0.0.0')
-    PORT = int(os.getenv('PORT', '5000'))  # Преобразуем порт в целое число
+    PORT = int(os.getenv('PORT', '5000'))
     app.run(host=HOST, port=PORT)
