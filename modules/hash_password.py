695ad01560e5441c894078c4ff2896a26d445f91
Kira <90696175+KiraPixel@users.noreply.github.com>
2024-08-31 17:54:55 +0300

    hash update

--- modules/hash_password.py
+++ modules/hash_password.py
@@ -5,7 +5,8 @@ import os
 
 def hash_password(password):
     SALT = os.getenv('SALT', b'r8\xcc\xf5\xf6\x8eq\xa3\xad\x0b8Y\xfc\x9f^\xaf')
-    SALT = SALT.encode('utf-8')
+    if not isinstance(SALT, bytes):
+        SALT = SALT.encode('utf-8')
     hasher = hashlib.pbkdf2_hmac('sha256', password.encode(), SALT, 100000)
     return binascii.hexlify(hasher).decode()
 
@@ -14,5 +15,3 @@ def compare_passwords(stored_hashed_password, password):
     hashed_password = hash_password(password)
     return stored_hashed_password == hashed_password
 
-
-hash_password('asdas')
