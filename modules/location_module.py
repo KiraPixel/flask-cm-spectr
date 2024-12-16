1463d508836644e8076bf26d4fa6cb3b9faf6904
Kira <90696175+KiraPixel@users.noreply.github.com>
2024-12-16 09:52:54 +0300

    Update location_module.py

--- modules/location_module.py
+++ modules/location_module.py
@@ -12,27 +12,40 @@ def get_address(x, y):
 
 
 def get_address_decorator(coords=None):
-    x, y = coords
-    if x == 0 or y == 0 or coords is None:
-        return ''
+    try:
+        if coords is None:
+            return ''
 
-    location = geolocator.reverse((x, y), exactly_one=True)
-    if location is None:
-        return 'Ошибка определения'
-    address = location.raw.get('address', {})
-    #print(address)
-    # Попробуем получить нужные части адреса
-    city = address.get('city') or address.get('state') or address.get('town') or address.get('state', '') or address.get('county')
+        x, y = coords
+        if x == 0 or y == 0:
+            return ''
 
-    road = address.get('road') or address.get('city_district', '') or address.get('county') or address.get(
-        'municipality') or address.get('suburb') or address.get('house') or address.get('industrial') or address.get(
-        'hamlet') or address.get('neighbourhood') or address.get('town')
+        location = geolocator.reverse((x, y), exactly_one=True)
+        if location is None:
+            return 'Ошибка определения'
 
-    house_number = address.get('house_number') or address.get('amenity') or address.get('village') or address.get('neighbourhood') or address.get(
-        'municipality') or address.get('postcode') or address.get("quarter") or address.get('road') or address.get("region")
+        address = location.raw.get('address', {})
 
-    # Формируем сокращенный адрес
-    short_address = f"{city}, {road}, {house_number}".strip(', ')
-    if short_address == 'Химки, Коммунальный проезд, 141410':
-        short_address = "Химки, Коммунальный проезд, 2"
-    return short_address
+        # Попробуем получить нужные части адреса
+        city = address.get('city') or address.get('state') or address.get('town') or \
+               address.get('county')
+
+        road = address.get('road') or address.get('city_district', '') or address.get('county') or \
+               address.get('municipality') or address.get('suburb') or address.get('house') or \
+               address.get('industrial') or address.get('hamlet') or address.get('neighbourhood') or \
+               address.get('town')
+
+        house_number = address.get('house_number') or address.get('amenity') or address.get('village') or \
+                       address.get('neighbourhood') or address.get('municipality') or address.get('postcode') or \
+                       address.get("quarter") or address.get('road') or address.get("region")
+
+        # Формируем сокращенный адрес
+        short_address = f"{city}, {road}, {house_number}".strip(', ')
+        if short_address == 'Химки, Коммунальный проезд, 141410':
+            short_address = "Химки, Коммунальный проезд, 2"
+
+        return short_address
+
+    except Exception as e:
+        # print(f"Error occurred: {e}")
+        return 'Ошибка определения адреса'
