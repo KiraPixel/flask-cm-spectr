5c6cf0d4884941d89901990545dd22474faafc65
Kira <90696175+KiraPixel@users.noreply.github.com>
2024-12-21 02:59:34 +0300

    Mobile update | Add PWA

---
+++ app/static/service-worker.js
@@ -0,0 +1,21 @@
+// static/js/service-worker.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/static/img/logo.png',
+                '/static/css/styles.css',
+                '/static/js/main.js',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
