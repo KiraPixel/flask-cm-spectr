3f328026bac9eaa504fcbe49f0e040e80b636e2e
Kira <90696175+KiraPixel@users.noreply.github.com>
2025-05-31 23:55:42 +0300

    fix

--- modules/mail_sender.py
+++ modules/mail_sender.py
@@ -2,17 +2,19 @@ import os
 from exchangelib import Credentials, Account, Message, DELEGATE, HTMLBody, FileAttachment, Configuration
 
 sender_email = os.getenv('MAIL_MAIL', 'nomail@nomail')
-sender_login = os.getenv('MAIL_USERNAME', 'user')
+sender_user = os.getenv('MAIL_USERNAME', 'user')
+sender_domain = os.getenv('MAIL_USERNAME_DOMAIN', 'mail')
 sender_password = os.getenv('MAIL_PASSWORD', 'password')
 sender_host = os.getenv('MAIL_HOST', 'mail.ru')
 
+full_username = f"{sender_domain}\\{sender_user}"
 
 def send_email(target_email, subject, body, attachment_name=None, attachment_content=None):
     # try:
     # Создаем учетные данные
     config = Configuration(
         server=sender_host,
-        credentials=Credentials(username=sender_login, password=sender_password)
+        credentials=Credentials(username=full_username, password=sender_password)
     )
 
     # Подключаемся к учетной записи
@@ -26,7 +28,7 @@ def send_email(target_email, subject, body, attachment_name=None, attachment_con
         body=HTMLBody(body),
         to_recipients=[target_email]
     )
-    print(f'Send email to {target_email} with subject "{subject}" from "{sender_email}" login {sender_login}')
+    print(f'Send email to {target_email} with subject "{subject}" from "{sender_email}" login {full_username}')
     if attachment_name:
         attachment = FileAttachment(
             name=attachment_name,
