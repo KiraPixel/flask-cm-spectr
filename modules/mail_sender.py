530e081236f429167159fe7c4373ac4288117c43
Kira <90696175+KiraPixel@users.noreply.github.com>
2025-06-01 02:00:24 +0300

    return try

--- modules/mail_sender.py
+++ modules/mail_sender.py
@@ -16,37 +16,37 @@ else:
 
 def send_email(target_email, subject, body, attachment_name=None, attachment_content=None):
     print(f'Mail format: {full_username}')
-    # try:
+    try:
     # Создаем учетные данные
-    config = Configuration(
-        server=sender_host,
-        credentials=Credentials(username=full_username, password=sender_password)
-    )
-
-    # Подключаемся к учетной записи
-    account = Account(sender_email, config=config, autodiscover=False, access_type=DELEGATE)
-
-    # Создаем сообщение
-    msg = Message(
-        account=account,
-        folder=account.sent,
-        subject=subject,
-        body=HTMLBody(body),
-        to_recipients=[target_email]
-    )
-    print(f'Send email to {target_email} with subject "{subject}" from "{sender_email}" login {full_username}')
-    if attachment_name:
-        attachment = FileAttachment(
-            name=attachment_name,
-            content=attachment_content,
+        config = Configuration(
+            server=sender_host,
+            credentials=Credentials(username=full_username, password=sender_password)
         )
-        msg.attach(attachment)
 
-    # Отправляем сообщение
-    msg.send()
-    return True
-    # except Exception as e:
-    #     print(f'Произошла ошибка при отправке сообщения: {e}')
-    #     return False
+        # Подключаемся к учетной записи
+        account = Account(sender_email, config=config, autodiscover=False, access_type=DELEGATE)
+
+        # Создаем сообщение
+        msg = Message(
+            account=account,
+            folder=account.sent,
+            subject=subject,
+            body=HTMLBody(body),
+            to_recipients=[target_email]
+        )
+        print(f'Send email to {target_email} with subject "{subject}" from "{sender_email}" login {full_username}')
+        if attachment_name:
+            attachment = FileAttachment(
+                name=attachment_name,
+                content=attachment_content,
+            )
+            msg.attach(attachment)
+
+        # Отправляем сообщение
+        msg.send()
+        return True
+    except Exception as e:
+        print(f'Произошла ошибка при отправке сообщения: {e}')
+        return False
 
 
