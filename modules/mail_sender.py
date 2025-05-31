ffaf0804ecffa8e380f07091e258f8be75c08caa
Kira <90696175+KiraPixel@users.noreply.github.com>
2025-05-31 23:15:31 +0300

    debug

--- modules/mail_sender.py
+++ modules/mail_sender.py
@@ -8,34 +8,34 @@ sender_password = os.getenv('MAIL_PASSWORD', 'password')
 
 def send_email(target_email, subject, body, attachment_name=None, attachment_content=None):
     print(sender_email, sender_login, sender_password)
-    try:
-        # Создаем учетные данные
-        credentials = Credentials(username=sender_login, password=sender_password)
+    #try:
+    # Создаем учетные данные
+    credentials = Credentials(username=sender_login, password=sender_password)
 
-        # Подключаемся к учетной записи
-        account = Account(sender_email, credentials=credentials, autodiscover=True, access_type=DELEGATE)
+    # Подключаемся к учетной записи
+    account = Account(sender_email, credentials=credentials, autodiscover=True, access_type=DELEGATE)
 
-        # Создаем сообщение
-        msg = Message(
-            account=account,
-            folder=account.sent,
-            subject=subject,
-            body=HTMLBody(body),
-            to_recipients=[target_email]
+    # Создаем сообщение
+    msg = Message(
+        account=account,
+        folder=account.sent,
+        subject=subject,
+        body=HTMLBody(body),
+        to_recipients=[target_email]
+    )
+    print(f'Send email to {target_email} with subject "{subject}" from "{sender_email}" login {sender_login}')
+    if attachment_name:
+        attachment = FileAttachment(
+            name=attachment_name,
+            content=attachment_content,
         )
-        print(f'Send email to {target_email} with subject "{subject}" from "{sender_email}" login {sender_login}')
-        if attachment_name:
-            attachment = FileAttachment(
-                name=attachment_name,
-                content=attachment_content,
-            )
-            msg.attach(attachment)
+        msg.attach(attachment)
 
-        # Отправляем сообщение
-        msg.send()
-        return True
-    except Exception as e:
-        print(e)
-        return False
+    # Отправляем сообщение
+    msg.send()
+    return True
+    # except Exception as e:
+    #     print(e)
+    #     return False
 
 
